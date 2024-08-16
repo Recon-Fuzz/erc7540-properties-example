4f00c2bb6dde2e630f53adf8e0821f104852b9dd
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-08-16 11:16:13 -0400

    refactor: fixing broken properties

--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -4,13 +4,31 @@ pragma solidity ^0.8.0;
 import {Asserts} from "@chimera/Asserts.sol";
 import {Setup} from "./Setup.sol";
 import {ERC7540Properties} from "erc7540-reusable-properties/ERC7540Properties.sol";
+import {CallTestAndUndo} from "./helpers/CallTestAndUndo.sol";
 
-abstract contract Properties is Setup, Asserts, ERC7540Properties {
-    function crytic_erc7540_1() public returns (bool test) {
-        test = erc7540_1(address(vault));
+abstract contract Properties is
+    Setup,
+    Asserts,
+    ERC7540Properties,
+    CallTestAndUndo
+{
+    function erc7540_1_call_target() public returns (bool) {
+        bytes memory encoded = abi.encodeCall(this.erc7540_1, (address(vault)));
+        bool asBool = _doTestAndReturnResult(encoded);
+
+        /// NOTE: define this as an assetion so it can be tested in assertion mode
+        t(asBool, "erc7540_1");
+
+        return asBool;
     }
 
-    function crytic_erc7540_2() public returns (bool test) {
-        test = erc7540_2(address(vault));
+    function erc7540_2_call_target() public returns (bool) {
+        bytes memory encoded = abi.encodeCall(this.erc7540_2, (address(vault)));
+        bool asBool = _doTestAndReturnResult(encoded);
+
+        /// NOTE: define this as an assetion so it can be tested in assertion mode
+        t(asBool, "erc7540_2");
+
+        return asBool;
     }
 }
